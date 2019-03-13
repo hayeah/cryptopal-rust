e4fdf8e3484aef852030081e1ed384fd30686498
Howard Yeh <hayeah@gmail.com>
2019-03-13 23:01:05 +0800

    reimplement e03

--- e03/src/main.rs
+++ e03/src/main.rs
@@ -1,75 +1,106 @@
+#[macro_use]
+extern crate lazy_static;
+
 extern crate hex;
 
-use std::cmp::Ord;
-use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::error::Error;
 
-fn xor(dst: &mut Vec<u8>, src: &Vec<u8>, mask: u8) {
+fn xor(dst: &mut [u8], src: &[u8], mask: u8) {
     for i in 0..src.len() {
         dst[i] = src[i] ^ mask;
     }
 }
 
-#[derive(Eq)]
+#[derive(PartialEq, PartialOrd)]
 struct DecodeResult {
+    score: f64,
     key: u8,
     ptext: String,
-    score: i64,
 }
 
-impl Ord for DecodeResult {
-    fn cmp(&self, other: &DecodeResult) -> Ordering {
-        // self.height.cmp(&other.height)
-        return self.score.cmp(&other.score);
-    }
+lazy_static! {
+    static ref LETTERS_FREQ_TUPLES: Vec<(char, f64)> = {
+        [
+            ('e', 12.49),
+            ('t', 9.28),
+            ('a', 8.04),
+            ('o', 7.64),
+            ('i', 7.57),
+            ('n', 7.23),
+            ('s', 6.51),
+            ('r', 6.28),
+            ('h', 5.05),
+            ('l', 4.07),
+            ('d', 3.82),
+            ('c', 3.34),
+            ('u', 2.73),
+            ('m', 2.51),
+            ('f', 2.40),
+            ('p', 2.14),
+            ('g', 1.87),
+            ('w', 1.68),
+            ('y', 1.66),
+            ('b', 1.48),
+            ('v', 1.05),
+            ('k', 5.4),
+            ('x', 2.3),
+            ('j', 1.6),
+            ('q', 1.2),
+            ('z', 9.0),
+        ]
+        .to_vec()
+    };
+    static ref LETTERS_FREQ: HashMap<char, f64> = { LETTERS_FREQ_TUPLES.iter().cloned().collect() };
 }
 
-impl PartialOrd for DecodeResult {
-    fn partial_cmp(&self, other: &DecodeResult) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
+// A string that matches the expected english letters frequency has score of 0.
+// The higher the score, the less english it looks.
+fn non_english_score(s: &str) -> f64 {
+    let mut char_counts: HashMap<char, i64> = HashMap::new();
 
-impl PartialEq for DecodeResult {
-    fn eq(&self, other: &DecodeResult) -> bool {
-        self.score == other.score
+    for ch in s.chars() {
+        match char_counts.get_mut(&ch) {
+            Some(count) => {
+                *count += 1;
+            }
+            _ => {
+                char_counts.insert(ch, 1);
+            }
+        }
     }
+
+    let len = s.len() as f64;
+    let mut score: f64 = 0.0;
+
+    for (ch, count) in char_counts.iter() {
+        // scoring for English-like text
+        if !(ch.is_alphanumeric() || ch.is_whitespace() || ch.is_ascii_punctuation()) {
+            score += (*count as f64) * 1000.0;
+            // score += *count as f64;
+            continue;
+        }
+
+        if ch.is_ascii_punctuation() {
+            // give punctuation a slight penalty
+            score += 100.0;
+            continue;
+        }
+
+        let freq = len / (*count as f64);
+        let english_freq: f64 = match LETTERS_FREQ.get(ch) {
+            Some(freq) => *freq,
+            None => 0.0f64,
+        };
+
+        let diff = freq - english_freq;
+        score += (diff * diff).sqrt();
+    }
+
+    return score;
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let freq_table: HashMap<char, i64> = [
-        ('e', 1249),
-        ('t', 928),
-        ('a', 804),
-        ('o', 764),
-        ('i', 757),
-        ('n', 723),
-        ('s', 651),
-        ('r', 628),
-        ('h', 505),
-        ('l', 407),
-        ('d', 382),
-        ('c', 334),
-        ('u', 273),
-        ('m', 251),
-        ('f', 240),
-        ('p', 214),
-        ('g', 187),
-        ('w', 168),
-        ('y', 166),
-        ('b', 148),
-        ('v', 105),
-        ('k', 54),
-        ('x', 23),
-        ('j', 16),
-        ('q', 12),
-        ('z', 9),
-    ]
-    .iter()
-    .cloned()
-    .collect();
-
     let input =
         hex::decode("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")?;
 
@@ -79,34 +110,12 @@ fn main() -> Result<(), Box<dyn Error>> {
         // a-z
         let mut buf = vec![0; input.len()];
         xor(&mut buf, &input, c);
-        // let r = String::from_utf8(buf[0..buf.len() - 2].to_vec());
         let r = String::from_utf8(buf);
 
         if r.is_ok() {
             let s = r.unwrap();
 
-            let mut score: i64 = 0;
-
-            for ch in s.chars() {
-                // scoring for English-like text
-                if !ch.is_ascii() {
-                    score -= 10000; // penalty for non-ascii char
-                    continue;
-                }
-
-                if ch.is_control() {
-                    score -= 10000;
-                    continue;
-                }
-
-                let kchar = ch.to_ascii_lowercase();
-                let f = freq_table.get(&kchar);
-                if f.is_some() {
-                    score += f.unwrap();
-                } else {
-                    score -= 1000; // penalty for non-letter char
-                }
-            }
+            let score = non_english_score(&s);
 
             results.push(DecodeResult {
                 key: c,
@@ -116,7 +125,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    results.sort();
+    results.sort_by(|a, b| b.partial_cmp(&a).unwrap());
 
     for r in results {
         println!("key={} score={} ptext={:?}", r.key, r.score, r.ptext);
