50bb6a6f8fae182c06144da417115550fee8c775
Howard Yeh <hayeah@gmail.com>
2019-03-16 11:15:44 +0800

    format

--- e04/src/main.rs
+++ e04/src/main.rs
@@ -12,43 +12,43 @@ use std::io::BufReader;
 
 #[derive(Fail, Debug)]
 enum Error {
-    #[fail(display = "Invalid hex string: {}", _0)]
-    InvalidHexErr(hex::FromHexError),
-    #[fail(display = "No English plain text is found for line {}", _0)]
-    NotFoundErr(usize),
+  #[fail(display = "Invalid hex string: {}", _0)]
+  InvalidHexErr(hex::FromHexError),
+  #[fail(display = "No English plain text is found for line {}", _0)]
+  NotFoundErr(usize),
 }
 
 impl From<hex::FromHexError> for Error {
-    fn from(e: hex::FromHexError) -> Error {
-        Error::InvalidHexErr(e)
-    }
+  fn from(e: hex::FromHexError) -> Error {
+    Error::InvalidHexErr(e)
+  }
 }
 
 fn crack(line: &str, lineno: usize) -> Result<DecodeResult, Error> {
-    let ctext = hex::decode(line)?;
-    let mut cracker = Cracker::new(ctext);
-    match cracker.best_result() {
-        Some(result) => {
-            if result.score < 1000.0 {
-                return Ok(result);
-            } else {
-                return Err(Error::NotFoundErr(lineno));
-            }
-        }
-        None => Err(Error::NotFoundErr(lineno)),
+  let ctext = hex::decode(line)?;
+  let mut cracker = Cracker::new(&ctext);
+  match cracker.best_result() {
+    Some(result) => {
+      if result.score < 1000.0 {
+        return Ok(result);
+      } else {
+        return Err(Error::NotFoundErr(lineno));
+      }
     }
+    None => Err(Error::NotFoundErr(lineno)),
+  }
 }
 
 fn main() -> Result<(), failure::Error> {
-    let f = File::open("4.txt")?;
+  let f = File::open("4.txt")?;
 
-    let r = BufReader::new(f);
+  let r = BufReader::new(f);
 
-    for (i, line) in r.lines().enumerate() {
-        if let Ok(result) = crack(&line?, i + 1) {
-            println!("BINGO: line={} {}", i + 1, result);
-        }
+  for (i, line) in r.lines().enumerate() {
+    if let Ok(result) = crack(&line?, i + 1) {
+      println!("BINGO: line={} {}", i + 1, result);
     }
+  }
 
-    return Ok(());
+  return Ok(());
 }
--- e06/src/main.rs
+++ e06/src/main.rs
@@ -10,149 +10,149 @@ use cryptopal::single_char;
 use std::rc::Rc;
 
 fn hamming_score(data: &[u8], keysize: usize) -> f64 {
-    let n = data.len() / keysize;
+  let n = data.len() / keysize;
 
-    // Compute the average of hamming edit distance between blocks
-    let mut score: f64 = 0.0;
+  // Compute the average of hamming edit distance between blocks
+  let mut score: f64 = 0.0;
 
-    for i in 0..n - 1 {
-        let block1 = &data[i * keysize..(i + 1) * keysize];
-        let block2 = &data[(i + 1) * keysize..(i + 2) * keysize];
+  for i in 0..n - 1 {
+    let block1 = &data[i * keysize..(i + 1) * keysize];
+    let block2 = &data[(i + 1) * keysize..(i + 2) * keysize];
 
-        let d = hamming::distance(block1, block2);
+    let d = hamming::distance(block1, block2);
 
-        // normalize hamming distance by length of blocksize
-        score += d as f64 / keysize as f64;
-    }
+    // normalize hamming distance by length of blocksize
+    score += d as f64 / keysize as f64;
+  }
 
-    return score / n as f64;
+  return score / n as f64;
 }
 
 #[derive(PartialEq, PartialOrd, Debug)]
 struct KeySizeResult {
-    score: f64,
-    keysize: usize,
+  score: f64,
+  keysize: usize,
 }
 
 impl Eq for KeySizeResult {}
 
 impl Ord for KeySizeResult {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.partial_cmp(other).unwrap()
-    }
+  fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+    self.partial_cmp(other).unwrap()
+  }
 }
 
 struct KeySizeAnalyzer {
-    // data: &'a [u8],
-    data: Rc<Vec<u8>>,
-    range: std::ops::Range<usize>,
+  // data: &'a [u8],
+  data: Rc<Vec<u8>>,
+  range: std::ops::Range<usize>,
 }
 
 // TODO Can I rewrite this with lifetime?
 impl KeySizeAnalyzer {
-    fn new(data: Rc<Vec<u8>>, range: std::ops::Range<usize>) -> KeySizeAnalyzer {
-        KeySizeAnalyzer {
-            data: data,
-            range: range,
-        }
+  fn new(data: Rc<Vec<u8>>, range: std::ops::Range<usize>) -> KeySizeAnalyzer {
+    KeySizeAnalyzer {
+      data: data,
+      range: range,
     }
+  }
 
-    fn iter(&self) -> impl Iterator<Item = KeySizeResult> {
-        let data = self.data.clone();
-        return self
-            .range
-            .clone()
-            .into_iter()
-            .map(move |keysize| KeySizeResult {
-                keysize: keysize,
-                score: hamming_score(&data, keysize),
-            });
-    }
+  fn iter(&self) -> impl Iterator<Item = KeySizeResult> {
+    let data = self.data.clone();
+    return self
+      .range
+      .clone()
+      .into_iter()
+      .map(move |keysize| KeySizeResult {
+        keysize: keysize,
+        score: hamming_score(&data, keysize),
+      });
+  }
 }
 
 impl IntoIterator for KeySizeAnalyzer {
-    type Item = KeySizeResult;
+  type Item = KeySizeResult;
 
-    type IntoIter = Box<dyn Iterator<Item = Self::Item>>;
+  type IntoIter = Box<dyn Iterator<Item = Self::Item>>;
 
-    fn into_iter(self) -> Self::IntoIter {
-        Box::new(self.iter())
-    }
+  fn into_iter(self) -> Self::IntoIter {
+    Box::new(self.iter())
+  }
 }
 
 fn block_column(buf: &mut Vec<u8>, data: &[u8], blocksize: usize, col: usize) {
-    buf.truncate(0);
-    let mut i = 0 + col;
-    while i < data.len() {
-        buf.push(data[i]);
-        i += blocksize;
-    }
+  buf.truncate(0);
+  let mut i = 0 + col;
+  while i < data.len() {
+    buf.push(data[i]);
+    i += blocksize;
+  }
 }
 
 #[derive(Debug)]
 struct CrackResult {
-    score: i64,
-    key: String,
-    ptext: String,
+  score: i64,
+  key: String,
+  ptext: String,
 }
 
 fn xor_bytes(buf: &mut [u8], key: &[u8]) {
-    for (i, byte) in buf.iter_mut().enumerate() {
-        *byte = *byte ^ key[i % key.len()];
-    }
+  for (i, byte) in buf.iter_mut().enumerate() {
+    *byte = *byte ^ key[i % key.len()];
+  }
 }
 
 fn crack(data: &[u8], blocksize: usize) -> Result<CrackResult, failure::Error> {
-    let mut block: Vec<u8> = vec![0; (data.len() / blocksize) + 1];
-    let mut key: Vec<u8> = vec![0; blocksize];
+  let mut block: Vec<u8> = vec![0; (data.len() / blocksize) + 1];
+  let mut key: Vec<u8> = vec![0; blocksize];
 
-    for col in 0..blocksize {
-        block_column(&mut block, data, blocksize, col);
+  for col in 0..blocksize {
+    block_column(&mut block, data, blocksize, col);
 
-        let mut cracker = single_char::Cracker::new(&block);
+    let mut cracker = single_char::Cracker::new(&block);
 
-        if let Some(result) = cracker.first(2000.0) {
-            // println!("score={}", result.score);
-            key[col] = result.key;
-        } else {
-            return Err(format_err!("no key found"));
-        }
+    if let Some(result) = cracker.first(2000.0) {
+      // println!("score={}", result.score);
+      key[col] = result.key;
+    } else {
+      return Err(format_err!("no key found"));
     }
+  }
 
-    let mut ptext_bytes: Vec<u8> = data.to_vec();
-    xor_bytes(&mut ptext_bytes, &key);
+  let mut ptext_bytes: Vec<u8> = data.to_vec();
+  xor_bytes(&mut ptext_bytes, &key);
 
-    let ptext = String::from_utf8(ptext_bytes)?;
+  let ptext = String::from_utf8(ptext_bytes)?;
 
-    return Ok(CrackResult {
-        score: cryptopal::english::nonfit_score(&ptext) as i64,
-        ptext: ptext,
-        key: String::from_utf8(key)?,
-    });
+  return Ok(CrackResult {
+    score: cryptopal::english::nonfit_score(&ptext) as i64,
+    ptext: ptext,
+    key: String::from_utf8(key)?,
+  });
 }
 
 fn main() -> Result<(), failure::Error> {
-    let data = Rc::new(read_base64_file("6.txt")?);
+  let data = Rc::new(read_base64_file("6.txt")?);
 
-    let keysize_analyzer = KeySizeAnalyzer::new(data.clone(), 2..40);
-    let mut keysize_results: Vec<KeySizeResult> = keysize_analyzer.into_iter().collect();
-    keysize_results.sort();
+  let keysize_analyzer = KeySizeAnalyzer::new(data.clone(), 2..40);
+  let mut keysize_results: Vec<KeySizeResult> = keysize_analyzer.into_iter().collect();
+  keysize_results.sort();
 
-    // for result in &keysize_results {
-    //     println!("{:?}", result);
-    // }
+  // for result in &keysize_results {
+  //     println!("{:?}", result);
+  // }
 
-    for result in keysize_results.iter().take(10) {
-        match crack(&data, result.keysize) {
-            Ok(bingo) => {
-                println!("{}", bingo.ptext);
-                println!("keysize={} key={:?}", result.keysize, bingo.key);
-            }
-            Err(err) => {
-                println!("keysize={} {}", result.keysize, err);
-            }
-        }
+  for result in keysize_results.iter().take(10) {
+    match crack(&data, result.keysize) {
+      Ok(bingo) => {
+        println!("{}", bingo.ptext);
+        println!("keysize={} key={:?}", result.keysize, bingo.key);
+      }
+      Err(err) => {
+        println!("keysize={} {}", result.keysize, err);
+      }
     }
+  }
 
-    return Ok(());
+  return Ok(());
 }
