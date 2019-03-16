f451a67f10250272a67216355d26c65a23d84d42
Howard Yeh <hayeah@gmail.com>
2019-03-16 09:50:37 +0800

    e03: fix

--- e03/src/main.rs
+++ e03/src/main.rs
@@ -7,11 +7,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     let input =
         hex::decode("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")?;
 
-    let mut cracker = Cracker::new(input);
+    let mut cracker = Cracker::new(&input);
     let results = cracker.best_results();
 
     // Print top 10 results
-    for r in &results[0..10] {
+    for r in &results[0..3] {
         println!("{}", r);
     }
 
